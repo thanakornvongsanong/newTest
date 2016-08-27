b04bbf3a3d8d059533d85da822b80ddc9b2b8d47
thanakornvongsanong <thanakorn.vongsanong@gmail.com>
2016-08-27 12:24:28 +0700

    Revert "1st"
    This reverts commit cf58e424715f4c4dc4e55102d4d167050336d564.

--- controller.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	include "model.php";
-	$username=$_GET['username'];
-	$password=$_GET['password'];
-	
-	header(loginSession($username,$password));
- ?>
--- registerController.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	include "model.php";
-	$fname = $_GET['fname'];
-	$lname = $_GET['lname'];
-
-	register($fname,$lname);
-
-
- ?>
